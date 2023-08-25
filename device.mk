17d1bf0aae55ec5079f8585d97977b4e29a995f0
EndCredits <endcredits@crepuscular-aosp.icu>
2023-08-25 21:09:01 +0800

    renoir: overlay: Move device-specific overlay to RRO
     * Also bumped device-specific overlay priority.
    
     * Accrouding to AOSP Documentation. We should configure bigger priority
       value for our device overlay to make sure the correspoending resources
       being overlayed correctly.
    
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>
    Signed-off-by: RobertGarciaa <chae0218@naver.com>

--- device.mk
+++ device.mk
@@ -24,16 +24,18 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
+
+PRODUCT_PACKAGES += \
+    FrameworksResOverlayRenoir \
+    SettingsProviderResOverlayRenoir \
+    SettingsResOverlayRenoir \
+    SystemUIResOverlayRenoir \
+    WifiResOverlayRenoir
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
-
-# WiFi
-PRODUCT_PACKAGES += \
-    TargetWifiOverlay
 
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/renoir/renoir-vendor.mk)
